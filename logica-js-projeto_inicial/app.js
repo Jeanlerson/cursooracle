dd65abf466cd86185544fb418888e8ace820e21b
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2025-07-23 15:17:03 -0300

    iniciando

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,10 @@
+alert("Olá Mundo!");
+
+let numeroSecreto = 29;
+let resposta = prompt("Escolha entre o número 1 e 30");
+
+if(numeroSecreto == resposta) {
+    console.log("Você acertou o número secreto! Parabéns!!");
+} else {
+    console.log("Você errou o número secreto. Tente novamente!")
+}

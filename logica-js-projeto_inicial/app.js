339d0980cf4bc7a798f481363971a3b7f2c3ecc7
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2025-07-24 16:29:54 -0300

    atividades-aula1.2

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,6 +1,6 @@
 // Curso Lógica de Programação:
 //  -mergulhe em programação com JavaScript
-
+/*
 alert("Olá Mundo!");
 
 let numeroSecreto = 8;
@@ -14,3 +14,35 @@ if(numeroSecreto == resposta) {
 
 console.log(resposta);
 console.log(numeroSecreto == resposta);
+*/
+
+//-------------ATV 1----------------
+
+alert("Olá! Tudo bem?");
+
+let respostaSemana = prompt("Qual o dia da semana?");
+let respostaMinuscula = respostaSemana.toLowerCase();
+
+let sab = "sábado";
+let dom = "domingo";
+
+if(respostaMinuscula == sab || respostaMinuscula == dom) {
+    alert("Tenha um bom final de semana!")
+} else {
+    alert("Tenha uma boa semana!")
+}
+
+
+//-------------ATV 2----------------
+let respNum = prompt("Digite um númer:")
+
+let number = Number(respNum)
+
+console.log(number)
+console.log(typeof number)
+
+if(number < 0) {
+    alert("Você digitou um número negativo")
+} else {
+    alert("Seu número é positivo")
+}

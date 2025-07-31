3264eaddcdf7d1d203d82a8bcb3d27cb7e7aa43d
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2025-07-31 16:44:53 -0300

    atv-aula1.1

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -7,3 +7,24 @@ paragraph.innerHTML = "Escolha um número entre 1 e 10";
 function checkKick() {
     console.log("ta funcionando");
 };
+
+
+// ------------Atividade 1.1-------------
+function alertInterative() {
+    alert("Eu amo JS");
+};
+
+// ------------Atividade 1.2-------------
+function dialog() {
+    let city = prompt("Digite uma cidade:");
+    alert(`Estive em ${city} e lembrei de você`);
+};
+
+// ------------Atividade 1.2-------------
+function somaButton() {
+    let number1 = parseInt(prompt("Digitie um número inteiro:"));
+    let number2 = parseInt(prompt("Digitie outro número inteiro:"));
+    let resultSoma = number1 + number2;
+
+    alert(`O resultado da soma dos números é: ${resultSoma}`)
+}

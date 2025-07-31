6bdc8a10c0037386580d8f30b5aba101409581de
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2025-07-31 16:27:29 -0300

    aula2.1-

---
+++ js-curso-2-aula1/app.js
@@ -0,0 +1,9 @@
+let title = document.querySelector("h1");
+let paragraph = document.querySelector("p");
+
+title.innerHTML = "Número Secreto";
+paragraph.innerHTML = "Escolha um número entre 1 e 10";
+
+function checkKick() {
+    console.log("ta funcionando");
+};
